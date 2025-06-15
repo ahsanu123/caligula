59c9fa6043e7c6d32d6e5bd1759b751abdabcc0b
Ahsanu <caasperahsanuamala5@gmail.com>
2025-06-15 22:33:46 +0700

    [06-15-2233] make signal poc, not tested yet,

--- firmware/src/bin/poc_embassy_sync_signal.rs
+++ firmware/src/bin/poc_embassy_sync_signal.rs
@@ -2,25 +2,85 @@
 #![no_main]
 
 // ====================================================
-// Signal - Signalling latest value to a single consumer.
+// Signal - Signaling latest value to a single consumer.
+//
+// producer will make apple every second,
+// it will saved on buffer. on every 10 apples,
+// producer will signaling batch consumer to take
+// the apples.
+//
 // ====================================================
+
+use core::cell::RefCell;
+
+use alloc::vec::Vec;
+use defmt::Format;
+use embassy_sync::signal::Signal;
+use esp_hal::rng::Rng;
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::mutex::Mutex;
 use esp_backtrace as _;
-use esp_hal::{
-    gpio::{Level, Output, OutputConfig},
-    timer::timg::TimerGroup,
-};
+use esp_hal::timer::timg::TimerGroup;
+use static_cell::StaticCell;
 
 extern crate alloc;
 
+#[derive(Format, Clone)]
+pub struct Apple {
+    pub weight: u32,
+    pub diameter: u32,
+}
+
+impl Apple {
+    pub fn new(weight: u32, diameter: u32) -> Self {
+        Self { weight, diameter }
+    }
+}
+
+enum ConsumeCommand {
+    EatNow,
+    NotFilled,
+}
+
+type SharedRNG = Mutex<CriticalSectionRawMutex, Rng>;
+type SharedAppleType = RefCell<Vec<Apple>>;
+
+static RNG_MUTEX: StaticCell<SharedRNG> = StaticCell::new();
+static APPLES: StaticCell<SharedAppleType> = StaticCell::new();
+static EAT_SIGNAL: Signal<CriticalSectionRawMutex, ConsumeCommand> = Signal::new();
+
 #[embassy_executor::task]
-async fn run() {
+async fn apple_producer(rng_mutex: &'static SharedRNG, apples: &'static SharedAppleType) {
     loop {
-        defmt::println!("Hello World from Task");
+        let mut rng_lock = rng_mutex.lock().await;
+        let apple = Apple::new(rng_lock.random(), rng_lock.random());
+        let apple_len = apples.borrow().len();
+        apples.borrow_mut().push(apple.clone());
+
+        defmt::println!("producing apple {:?}, {}", apple, apple_len);
         Timer::after(Duration::from_millis(1_000)).await
+    }
+}
+
+#[embassy_executor::task]
+async fn apple_consumer(apples: &'static SharedAppleType) {
+    loop {
+        let command = EAT_SIGNAL.wait().await;
+
+        if let ConsumeCommand::EatNow = command {
+            defmt::println!(
+                "consuming batch apples, {:?}",
+                apples.borrow().first_chunk::<1>()
+            );
+            defmt::println!("emptyng the apple!!");
+            apples.borrow_mut().clear();
+        }
+
+        Timer::after(Duration::from_millis(500)).await
     }
 }
 
@@ -34,19 +94,11 @@ async fn main(spawner: Spawner) {
     let timer_group_zero = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timer_group_zero.timer0);
 
-    let mut led1 = Output::new(peripherals.GPIO32, Level::Low, OutputConfig::default());
-    led1.set_high();
+    let rng = Rng::new(peripherals.RNG);
 
-    let mut led2 = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
-    led2.set_low();
+    let rng_mutex = RNG_MUTEX.init(Mutex::new(rng));
+    let apples_cell = APPLES.init(RefCell::new(Vec::new()));
 
-    spawner.spawn(run()).ok();
-
-    loop {
-        led1.toggle();
-        led2.toggle();
-
-        defmt::println!("Togling Led");
-        Timer::after(Duration::from_millis(3_000)).await;
-    }
+    spawner.spawn(apple_producer(rng_mutex, apples_cell)).ok();
+    spawner.spawn(apple_consumer(apples_cell)).ok();
 }
